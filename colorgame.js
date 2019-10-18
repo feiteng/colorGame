b4fff665a1eb13f5af435dc3e589b14085ae6d74
Feiteng <li.feiteng@gmail.com>
2019-10-18 17:34:57 -0400

    update

--- colorgame.js
+++ colorgame.js
@@ -1,20 +1,34 @@
 window.onload=function() {
-	var colors = [
-	"rgb(255, 255, 0)",
-	"rgb(255, 0, 255)",
-	"rgb(255, 255, 255)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 0, 255)"
-	]
+	var colors = generateRandomColor(6);
 
-	var pickedColor = colors[3];
+	var pickedColor = pickColor();
 
 	var squares = document.querySelectorAll(".square")
 
 	var colorDisplay = document.getElementById("colorDisplay")
 
 	var messageDisplay = document.querySelector("#message")
+
+	var h1 = document.querySelector("h1");
+
+	var resetButton = document.querySelector("#reset");
+
+	resetButton.addEventListener("click", function(){
+		// generate all new colors
+		generateRandomColor(6);
+		// pick new random color
+		pickedColor = pickColor();
+		// change colors display to picked of squares
+		colorDisplay.textContent = pickedColor;
+
+		h1.style.backgroundColor = '#232323';
+
+		for(var i = 0; i < squares.length; i++)
+		{
+		// add initial colors
+			squares[i].style.backgroundColor = colors[i];
+		}
+	});
 
 	colorDisplay.textContent = pickedColor;
 
@@ -27,13 +41,49 @@ window.onload=function() {
 		squares[i].addEventListener("click", function(){
 			// grab color or clicked color
 			var clickedColor = this.style.backgroundColor;
-
-			if(clickedColor === pickedColor) alert("Correct");
+			console.log(clickedColor, pickedColor);
+			if(clickedColor === pickedColor) 
+			{
+				
+				h1.style.backgroundColor = clickedColor;
+				messageDisplay.textContent = "Correct";
+				changeColor(clickedColor);
+				resetButton.textContent = 'Play Aagin?'
+			}
 			else {
 				this.style.backgroundColor = '#232323';
 				messageDisplay.textContent = 'Try Again';
 			}
 			// compare color to picked color
 		});
+	}
+
+	function changeColor(color){
+		// loop through all squares
+		// change each color to match given color
+		for(var i = 0; i < color.length; i++)
+		{
+			squares[i].style.backgroundColor = color;
+		}	
+	}
+
+	function pickColor() {
+		var rand = Math.floor(Math.random() * colors.length);
+		return colors[rand];
+	}
+
+	function generateRandomColor(len) {
+		var arr = [];
+		// add random colors to array
+		for(var i = 0; i < len; i++)
+			arr.push(randomColor());
+		return arr;
+	}
+
+	function randomColor(){
+		var r = Math.floor(Math.random() * 256);
+		var g = Math.floor(Math.random() * 256);
+		var b = Math.floor(Math.random() * 256);
+		return "rgb(" + r + ", " + g + ", " + b + ")";
 	}
 }
